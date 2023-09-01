42dd39cce10f4f48e80cdb67c3b72d769a832848
UpayanGhosh <upaya1231@gmail.com>
2023-09-01 09:54:37 +0530

    Final

--- Task1 - Number Guessing Game/randomNumberGame.cpp
+++ Task1 - Number Guessing Game/randomNumberGame.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
-#include <cstdlib> // For using the srand fucntion
-#include <ctime>   // For using the time function
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 int main()
 {
-    // We will generate a random number using the srand function which takes an unsigned integer as input. Now to generate a random number everytime the user runs the program we will use the time function to get a number.
-    srand(time(NULL));                     // Here we are passing NULL to get the current time from our desktop.
-    int randomNumber = (rand() % 100) + 1; // Gives us a random number between 1 - 100. We can set the limit of number according to our need.
+
+    srand(time(NULL));
+    int randomNumber = (rand() % 100) + 1;
     int guess, count = 0;
     cout << "Guess a number between 1 - 100: " << endl;
     while (guess != randomNumber)
